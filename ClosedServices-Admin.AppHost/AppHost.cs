917063a7284000b3002f0b014df56de3a9b5608a
Rob Quincey <rob.quincey@dorsetcouncil.gov.uk>
2026-09-24 15:38:33 +0100

    Add postgres to Aspire host

--- ClosedServices-Admin.AppHost/AppHost.cs
+++ ClosedServices-Admin.AppHost/AppHost.cs
@@ -1,5 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.ClosedServices_Admin_Web>("closedservices-admin-web");
+var postgres = builder.AddPostgres("postgres")
+    .WithPgAdmin();
+var postgresdb = postgres.AddDatabase("closedservices-admin");
+
+builder.AddProject<Projects.ClosedServices_Admin_Web>("closedservices-admin-web")
+    .WithReference(postgresdb);
 
 builder.Build().Run();
